e831b4691cb1c46ac886408147ed91936565d1a6
gdovale07 <ed12@kent.ac.uk>
2026-02-03 12:12:20 +0000

    adding crew details.

---
+++ fleet_manager.py
@@ -0,0 +1,4 @@
+name = ["James","George","Paul","Alex"]
+rank = ["crew","commander","lieutenant","sergeant"]
+division = ["enforcement","operations","defence","supervision"]
+crew_no = [101,102,103,104]
