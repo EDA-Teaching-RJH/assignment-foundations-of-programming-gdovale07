05761367cd1f47fc3a049ab2fd31348ab321b955
gdovale07 <ed12@kent.ac.uk>
2026-02-04 15:12:56 +0000

    bug fixes and append additions

--- fleet_manager.py
+++ fleet_manager.py
@@ -5,6 +5,7 @@ division = ["enforcement","operations","defence","supervision"]
 crew_no = [101,102,103,104]
 
 active = True
+
 # allows for user to view crew members in database
 def view_crew_members(name,rank,division,crew_no):
     option = input("what would you like to do today?")
@@ -12,14 +13,21 @@ def view_crew_members(name,rank,division,crew_no):
         print("crew member list:")
     for i in range(len(name)):
         print(name[i] + " - " + rank[i])
+
 # used for adding members to the database
 def add_new_member(name,rank,division,crew_no):
     newname = input("name of new member")
     newrank = input("rank of new member")
     newdiv = input("division of new member")
+    nextid = max(crew_no) + 1
+    name.append(newname)
+    rank.append(newrank)
+    division.append(newdiv)
+    crew_no.append(nextid)
     print("new member added")
+
 # updates member details for the user
-def update_member(name,rank,division,crew_no):
+def update_member(rank,crew_no):
             updateid = int(input("what is the member's ID number?"))
             position = crew_no.index(updateid)
             print(position)
@@ -27,58 +35,37 @@ def update_member(name,rank,division,crew_no):
             newrank = input("what rank would you like to change the member to?")
             rank[position] = newrank
             print("new details confirmed")
+
 # removes members from the crew
 def remove_crew(name,rank,division,crew_no):
-            removalcrew = int(input("what is the member's ID number?"))
-            position = crew_no.index(removalcrew)
-            name.pop(position)
-            rank.pop(position)
-            division.pop(position)
-            print("member successfully removed")
+    removalcrew = int(input("what is the member's ID number?"))
+    if removalcrew in crew_no:
+        position = crew_no.index(removalcrew)
+        name.pop(position)
+        rank.pop(position)
+        division.pop(position)
+        crew_no.pop(position)
+        print("member successfully removed")
+    else:
+        print("ID not found, please try again")
 
 def init_database():
-     
+    pass
 
 def display_menu():
-        # main menu initial screen
-def run_system_moonlith():
-    print("LOADING...")
-    print("FLEET MANAGER MENU")
-while True:
-        print("MENU")
-        print("1. View crew members")
-        print("2. Add new crew member")
-        print("3. update crew details")
-        print("4. remove crew member")
-        print("5. leave menu")
-# asks the user for input from the main menu display
-        option = input("what would you like to do today?")
-# directs user to option 1
-        if option == "1":
-            view_crew_members(name,rank,division,crew_no)
-# directs user to option 2
-        elif option == "2":
-            add_new_member(name,rank,division,crew_no)
-# directs user to option 3
-        elif option == "3":
-            update_member(name,rank,division,crew_no)
-# directs user to option 4
-        elif option == "4":
-           remove_crew(name,rank,division,crew_no)
-# directs user to option 5 and breaks the code from looping 
-        elif option == "5":
-            print("thank you for using this service, have a great day")
-            break
-# prevents invalid inputs from crashing the code
-        else:
-            print("invalid input, please try again")
+    pass
+
 def search_crew(name,rank,division,crew_no):
-     
+    pass
+    
 def filter_by_division(name,division):
-
+    pass
+     
 def calculate_payroll(rank):
+    pass
 
-def count_officers(rank):   
+def count_officers(rank):
+    pass
 
 # main menu initial screen
 def run_system_moonlith():
