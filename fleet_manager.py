65a4a443e9451c0f01a921ace3345677de1bd316
gdovale07 <ed12@kent.ac.uk>
2026-02-04 10:08:20 +0000

    adding active = true to initialise the main menu

--- fleet_manager.py
+++ fleet_manager.py
@@ -2,3 +2,5 @@ name = ["James","George","Paul","Alex"]
 rank = ["crew","commander","lieutenant","sergeant"]
 division = ["enforcement","operations","defence","supervision"]
 crew_no = [101,102,103,104]
+
+active = True
