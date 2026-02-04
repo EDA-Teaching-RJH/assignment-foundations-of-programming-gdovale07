02e3914718720f14aca0cf258578c1a4d398d61c
gdovale07 <ed12@kent.ac.uk>
2026-02-04 10:21:32 +0000

    added the while true function which will activate the menu once the code is running, followed by 5 options within the menu which will be followed up next

--- fleet_manager.py
+++ fleet_manager.py
@@ -8,4 +8,11 @@ active = True
 def run_system_moonlith():
     print("LOADING...")
     print("FLEET MANAGER MENU")
-    
+
+    while True:
+        print("MENU")
+        print("1. View crew members")
+        print("2. Add new crew member")
+        print("3. update crew details")
+        print("4. remove crew member")
+        print("5. leave menu")
